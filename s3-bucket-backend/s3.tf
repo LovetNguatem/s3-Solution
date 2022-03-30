2ec502ddd3493c9fa067dacbb944dcd703de6514
Lovet Nguatem <lovetnguatem@gmail.com>
2022-03-29 18:04:29 -0600

    added backend to provider

--- s3-bucket-backend/s3.tf
+++ s3-bucket-backend/s3.tf
@@ -1,21 +1,7 @@
-#########################################
-#create S3 bucket
-#########################################
-/*
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
+
+# remote backend. 
 
 
-provider "aws" {
-  region = var.region
-}
-*/
 
 module "s3_backend_bucket" {
   source         = "/Users/lovetnguatem/Documents/s3-Solution/s3-bucket-backend/s3Module/"
